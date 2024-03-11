63df95855b114918630de2c6388f911951affbcc
MetalHuman38 <kalejaib@roehampton.ac.uk>
2024-03-11 22:26:42 +0000

    tic-tac-toe

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # Screen
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 700
+HEIGHT = 700
 
 # background color
 BG_COLOR = (28, 170, 156)
@@ -8,13 +8,31 @@ BG_COLOR = (28, 170, 156)
 # Line color
 LINE_COLOR = (23, 145, 135)
 
+# cross color
+CROSS_COLOR = (66, 66, 66)
+
 # Line Width
 LINE_WIDTH = 10
+CIRCLE_WIDTH = 10
+CROSS_WIDTH = 15
 
 # Rows and columns
 ROWS = 3
 COLS = 3
 
-
-# Square size
+# Square SIZE
 SQSIZE = WIDTH // COLS
+
+# Constants for Doctors and Cancer
+EMPTY = 0
+DOCTOR = 1
+CANCER = 2
+
+# Draw Circle
+CIRCLE_COLOR = (239, 231, 200)
+
+# Radius
+RADIUS = SQSIZE // 6
+
+# offset
+OFFSET = 70
--- simulator.py
+++ simulator.py
@@ -3,9 +3,17 @@ import sys
 import numpy as np
 
 # Importing constants from constants.py
-from constants import (WIDTH, HEIGHT, BG_COLOR, ROWS, COLS, SQSIZE, LINE_COLOR, LINE_WIDTH)
-# from constants import *
-
+from constants import (WIDTH, HEIGHT,
+                       BG_COLOR, ROWS,
+                       COLS, SQSIZE,
+                       LINE_COLOR,
+                       LINE_WIDTH,
+                       CIRCLE_COLOR,
+                       CIRCLE_WIDTH,
+                       RADIUS,
+                       CROSS_COLOR,
+                       CROSS_WIDTH,
+                       OFFSET)
 
 # Initialize the pygame library
 pygame.init()
@@ -19,14 +27,17 @@ class Board:
         # Create a 3x3 matrix of zeros to represent initial state of the board
         self.squares = np.zeros((ROWS, COLS))
 
-    def mark_square(self, row, col, player):
-        self.squares[row][col] = player
+    def mark_square(self, row, col, doctor):
+        self.squares[row][col] = doctor
+
+    def empty_square(self, row, col):
+        return self.squares[row][col] == 0
 
 
 class Game:
     def __init__(self):
         self.board = Board()
-        self.DOCTOR = 1
+        self.player = 1  # player 1-cross #2-circles
         self.draw_lines()
 
     def draw_lines(self):
@@ -38,8 +49,28 @@ class Game:
         # Vertical lines
         for i in range(COLS):
             pygame.draw.line(screen, LINE_COLOR, (i * WIDTH / COLS, 0), (i * WIDTH / COLS, HEIGHT), LINE_WIDTH)
-        # Update the display
+
         pygame.display.update()
+
+    def draw_fig(self, row, col):
+        if self.player == 1:
+            # draw cross
+            # descending line
+            start_desc = (col * SQSIZE + OFFSET, row * SQSIZE + OFFSET)
+            end_desc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_desc, end_desc, CROSS_WIDTH)
+
+            # ascending line
+            start_asc = (col * SQSIZE + OFFSET, row * SQSIZE + SQSIZE - OFFSET)
+            end_asc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + OFFSET)
+            pygame.draw.line(screen, CROSS_COLOR, start_asc, end_asc, CROSS_WIDTH)
+
+        elif self.player == 2:
+            center = (col * WIDTH / COLS + WIDTH / COLS // 2, row * HEIGHT / ROWS + HEIGHT / ROWS // 2)
+            pygame.draw.circle(screen, CIRCLE_COLOR, center, RADIUS, CIRCLE_WIDTH)
+
+    def next_turn(self):
+        self.player = self.player % 2 + 1
 
 
 # Main loop
@@ -47,6 +78,7 @@ def main():
 
     # Create an instance of the Game class
     game = Game()
+    board = game.board
 
     while True:
         for event in pygame.event.get():
@@ -58,8 +90,12 @@ def main():
                 pos = event.pos
                 row = pos[1] // SQSIZE
                 col = pos[0] // SQSIZE
-                print(row, col)
-        game.draw_lines()
+
+                if board.empty_square(row, col):
+                    board.mark_square(row, col, game.player)
+                    game.draw_fig(row, col)
+                    game.next_turn()
+                    print(board.squares)
 
         pygame.display.update()
 
